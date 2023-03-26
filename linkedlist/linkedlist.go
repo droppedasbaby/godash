2cc2cd9f724a1be229cfd95c8a4a4bff0ddf676f
amrinder-spare <asgrewal@ualberta.ca>
2023-03-26 21:19:52 +0530

    Update linkedlist.go

--- linkedlist/linkedlist.go
+++ linkedlist/linkedlist.go
@@ -1,1 +1,18 @@
 package linkedlist
+
+// Node is a node in a doubly linked list.
+type Node[T any] struct {
+	Prev  *Node[T]
+	Value T
+	Next  *Node[T]
+}
+
+// LinkedList is a doubly linked list.
+type LinkedList[T any] struct {
+	Head *Node[T]
+	Tail *Node[T]
+}
+
+// FromSlice returns a linked list from a slice/array.
+
+// ToSlice returns a slice from a linked list.
