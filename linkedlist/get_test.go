7026ad44c687c7123ab2e5c3e25077e7f0958f7a
amrinder-spare <asgrewal@ualberta.ca>
2023-03-30 23:58:54 +0530

    get tests finished, no panic tests for now

---
+++ linkedlist/get_test.go
@@ -0,0 +1,80 @@
+package linkedlist_test
+
+import (
+	"godash/linkedlist"
+	"godash/utils"
+	"testing"
+)
+
+func TestGet(t *testing.T) {
+	t.Parallel()
+
+	type customType struct {
+		ID   int
+		Name string
+	}
+
+	e1 := customType{ID: 1, Name: "Alice"}
+	e2 := customType{ID: 2, Name: "Bob"}
+	e3 := customType{ID: 3, Name: "Charlie"}
+
+	single := []customType{e1}
+	singleNodes := []*linkedlist.Node[customType]{
+		{Prev: nil, Value: &single[0], Next: nil},
+	}
+	singleLL := linkedlist.LinkedList[customType]{}
+	linkedlist.AddLast[customType](&singleLL, singleNodes[0])
+
+	multiple := []customType{e1, e2, e3}
+	multipleNodes := []*linkedlist.Node[customType]{
+		{Prev: nil, Value: &multiple[0], Next: nil},
+		{Prev: nil, Value: &multiple[1], Next: nil},
+		{Prev: nil, Value: &multiple[2], Next: nil},
+	}
+	multipleLL := linkedlist.LinkedList[customType]{}
+	linkedlist.AddLast[customType](&multipleLL, multipleNodes[0])
+	linkedlist.AddLast[customType](&multipleLL, multipleNodes[1])
+	linkedlist.AddLast[customType](&multipleLL, multipleNodes[2])
+
+	testCases := []utils.GenericTestCase[
+		utils.TwoArgumentTestCasesArgsType[*linkedlist.LinkedList[customType], int], *linkedlist.Node[customType]]{
+		{
+			Name: "Single-element linked list, get element at index 0",
+			Args: utils.TwoArgumentTestCasesArgsType[*linkedlist.LinkedList[customType], int]{
+				A: &singleLL,
+				B: 0,
+			},
+			Want: singleNodes[0],
+		},
+		{
+			Name: "Multiple-element linked list, get element at index 0",
+			Args: utils.TwoArgumentTestCasesArgsType[*linkedlist.LinkedList[customType], int]{
+				A: &multipleLL,
+				B: 0,
+			},
+			Want: multipleNodes[0],
+		},
+		{
+			Name: "Multiple-element linked list, get element at index 1",
+			Args: utils.TwoArgumentTestCasesArgsType[*linkedlist.LinkedList[customType], int]{
+				A: &multipleLL,
+				B: 1,
+			},
+			Want: multipleNodes[1],
+		},
+		{
+			Name: "Multiple-element linked list, get element at index 2",
+			Args: utils.TwoArgumentTestCasesArgsType[*linkedlist.LinkedList[customType], int]{
+				A: &multipleLL,
+				B: 2,
+			},
+			Want: multipleNodes[2],
+		},
+	}
+
+	utils.RunTwoArgumentTestCases(t, "Get()",
+		func(ll *linkedlist.LinkedList[customType], index int) *linkedlist.Node[customType] {
+			return linkedlist.Get[customType](ll, index)
+		}, testCases)
+
+}
