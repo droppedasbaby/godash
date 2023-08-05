4c75dfd94c7664b4e8a31ee7d252f3dcd8eed943
GrewalAS <asgrewal@ualberta.ca>
2023-08-05 02:18:11 -0600

    Update create_release.sh

--- create_release.sh
+++ create_release.sh
@@ -2,7 +2,11 @@
 
 set -e
 if [ "${CIRCLE_BRANCH}" = "main" ]; then
-  gh release create "${CIRCLE_TAG}" --generate-notes
+  git fetch --tags
+  latestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
+  newTag=$(echo $latestTag | awk -F. '/v[0-9]+\.[0-9]+\.[0-9]+/{print $1"."$2"."$3+1}')
+  echo "Creating a new release with tag: $newTag"
+#  gh release create "${CIRCLE_TAG}" --generate-notes
 else
   echo "This is not the main branch, so no release will be created."
   exit 1
