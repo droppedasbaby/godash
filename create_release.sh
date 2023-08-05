3cabd2b1182d32a15280907044fc054dc8e416dc
GrewalAS <asgrewal@ualberta.ca>
2023-08-05 02:16:08 -0600

    Update create_release.sh

--- create_release.sh
+++ create_release.sh
@@ -2,7 +2,7 @@
 
 set -e
 if [ "${CIRCLE_BRANCH}" = "main" ]; then
-  gh release create --generate-notes
+  gh release create "${CIRCLE_TAG}" --generate-notes
 else
   echo "This is not the main branch, so no release will be created."
   exit 1
