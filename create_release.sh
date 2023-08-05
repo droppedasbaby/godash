7b9a09d7cb6e36c5c8dbb0d6f190ceba278c4aa3
GrewalAS <asgrewal@ualberta.ca>
2023-08-05 02:02:42 -0600

    Update create_release.sh

--- create_release.sh
+++ create_release.sh
@@ -3,7 +3,7 @@
 set -e
 if [ "${CIRCLE_BRANCH}" = "main" ]; then
   git fetch --tags
-  echo ${GITHUB_TOKEN} | gh auth login --with-token
+  gh auth login --with-token
   gh release create ${CIRCLE_TAG} --title "${CIRCLE_TAG}" --notes "Release ${CIRCLE_TAG}"
 else
   echo "This is not the main branch, so no release will be created."
