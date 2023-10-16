7b8f7ea0ddf9abd8907fc4eeae7ce7ba12487928
GrewalAS <asgrewal@ualberta.ca>
2023-10-15 20:34:11 -0600

    padding refactor, bugs and tests

--- strutil/pad.go
+++ strutil/pad.go
@@ -2,38 +2,57 @@ package strutil
 
 import "strings"
 
+func padHelper(s string, n int, p string, l bool, r bool) string {
+	if len(s) >= n || len(p) == 0 {
+		return s
+	}
+
+	b := strings.Builder{}
+	b.Grow(n)
+	pl := n - len(s)
+
+	if l && r {
+		pl /= 2
+	}
+
+	if l {
+		for i := 0; i < pl/len(p); i++ {
+			b.WriteString(p)
+		}
+		b.WriteString(p[:pl%len(p)])
+		b.WriteString(s)
+	}
+
+	if l && r {
+		b.WriteString(p[:pl%len(p)])
+	}
+
+	if !l && r {
+		b.WriteString(s)
+	}
+
+	if r {
+		for i := 0; i < pl/len(p); i++ {
+			b.WriteString(p)
+		}
+		b.WriteString(p[:pl%len(p)])
+	}
+
+	return b.String()
+}
+
 // PadLeft returns a string of length n that is filled with the string p on the left.
 // If the length of s is already greater than or equal to n, the original string is returned.
 // If n is negative, behavior is undefined.
 func PadLeft(s string, n int, p string) string {
-	if len(s) >= n || len(p) == 0 {
-		return s
-	}
-	r := strings.Builder{}
-	r.Grow(n)
-	for i := 0; i < (n-len(s))/len(p); i++ {
-		r.WriteString(p)
-	}
-	r.WriteString(strings.Repeat(p, (n-len(s))%len(p)))
-	r.WriteString(s)
-	return r.String()
+	return padHelper(s, n, p, true, false)
 }
 
 // PadRight returns a string of length n that is filled with the string p on the right.
 // If the length of s is already greater than or equal to n, the original string is returned.
 // If n is negative, behavior is undefined.
 func PadRight(s string, n int, p string) string {
-	if len(s) >= n || len(p) == 0 {
-		return s
-	}
-	r := strings.Builder{}
-	r.Grow(n)
-	r.WriteString(s)
-	for i := 0; i < (n-len(s))/len(p); i++ {
-		r.WriteString(p)
-	}
-	r.WriteString(strings.Repeat(p, (n-len(s))%len(p)))
-	return r.String()
+	return padHelper(s, n, p, false, true)
 }
 
 // Pad returns a string of length n that is centered and filled with the string p.
@@ -44,22 +63,27 @@ func Pad(s string, n int, p string) string {
 	if len(s) >= n || len(p) == 0 {
 		return s
 	}
-	r := strings.Builder{}
-	r.Grow(n)
-	paddingLen := (n - len(s)) / 2
-	extraPad := (n - len(s)) % 2
 
-	for i := 0; i < (paddingLen+extraPad)/len(p); i++ {
-		r.WriteString(p)
+	b := strings.Builder{}
+	b.Grow(n)
+
+	pl := (n - len(s)) / 2
+	ep := (n - len(s)) % 2
+
+	pll, plr := pl, pl
+	if ep == 1 {
+		pll++
 	}
-	r.WriteString(strings.Repeat(p, (paddingLen+extraPad)%len(p)))
 
-	r.WriteString(s)
-
-	for i := 0; i < paddingLen/len(p); i++ {
-		r.WriteString(p)
+	for i := 0; i < pll/len(p); i++ {
+		b.WriteString(p)
 	}
-	r.WriteString(strings.Repeat(p, paddingLen%len(p)))
+	b.WriteString(p[:pll%len(p)])
+	b.WriteString(s)
+	for i := 0; i < plr/len(p); i++ {
+		b.WriteString(p)
+	}
+	b.WriteString(p[:plr%len(p)])
 
-	return r.String()
+	return b.String()
 }
--- strutil/pad_test.go
+++ strutil/pad_test.go
@@ -1,1 +1,139 @@
 package strutil_test
+
+import (
+	"github.com/GrewalAS/godash/strutil"
+	"github.com/GrewalAS/godash/utils"
+	"testing"
+)
+
+func TestPadLeft(t *testing.T) {
+	t.Parallel()
+
+	testCases := []utils.GenericTestCase[utils.ThreeArgumentTestCasesArgsType[string, int, string], string]{
+		{
+			Name: "Pad single character left",
+			Args: utils.ThreeArgumentTestCasesArgsType[string, int, string]{A: "abc", B: 5, C: "0"},
+			Want: "00abc",
+		},
+		{
+			Name: "Pad multiple characters left",
+			Args: utils.ThreeArgumentTestCasesArgsType[string, int, string]{A: "abc", B: 6, C: "xy"},
+			Want: "xyxabc",
+		},
+		{
+			Name: "Original string longer than padding",
+			Args: utils.ThreeArgumentTestCasesArgsType[string, int, string]{A: "abc", B: 2, C: "0"},
+			Want: "abc",
+		},
+		{
+			Name: "Empty original string",
+			Args: utils.ThreeArgumentTestCasesArgsType[string, int, string]{A: "", B: 2, C: "0"},
+			Want: "00",
+		},
+		{
+			Name: "Empty padding string",
+			Args: utils.ThreeArgumentTestCasesArgsType[string, int, string]{A: "abc", B: 5, C: ""},
+			Want: "abc",
+		},
+		{
+			Name: "Empty input and padding",
+			Args: utils.ThreeArgumentTestCasesArgsType[string, int, string]{A: "", B: 0, C: ""},
+			Want: "",
+		},
+		{
+			Name: "Negative length",
+			Args: utils.ThreeArgumentTestCasesArgsType[string, int, string]{A: "abc", B: -1, C: "0"},
+			Want: "abc",
+		},
+	}
+
+	utils.RunThreeArgumentTestCases(t, "PadLeft", strutil.PadLeft, testCases)
+}
+
+func TestPadRight(t *testing.T) {
+	t.Parallel()
+
+	testCases := []utils.GenericTestCase[utils.ThreeArgumentTestCasesArgsType[string, int, string], string]{
+		{
+			Name: "Pad single character right",
+			Args: utils.ThreeArgumentTestCasesArgsType[string, int, string]{A: "abc", B: 5, C: "0"},
+			Want: "abc00",
+		},
+		{
+			Name: "Pad multiple characters right",
+			Args: utils.ThreeArgumentTestCasesArgsType[string, int, string]{A: "abc", B: 6, C: "xy"},
+			Want: "abcxyx",
+		},
+		{
+			Name: "Original string longer than padding",
+			Args: utils.ThreeArgumentTestCasesArgsType[string, int, string]{A: "abc", B: 2, C: "0"},
+			Want: "abc",
+		},
+		{
+			Name: "Empty original string",
+			Args: utils.ThreeArgumentTestCasesArgsType[string, int, string]{A: "", B: 2, C: "0"},
+			Want: "00",
+		},
+		{
+			Name: "Empty padding string",
+			Args: utils.ThreeArgumentTestCasesArgsType[string, int, string]{A: "abc", B: 5, C: ""},
+			Want: "abc",
+		},
+		{
+			Name: "Empty input and padding",
+			Args: utils.ThreeArgumentTestCasesArgsType[string, int, string]{A: "", B: 0, C: ""},
+			Want: "",
+		},
+		{
+			Name: "Negative length",
+			Args: utils.ThreeArgumentTestCasesArgsType[string, int, string]{A: "abc", B: -1, C: "0"},
+			Want: "abc",
+		},
+	}
+
+	utils.RunThreeArgumentTestCases(t, "PadRight", strutil.PadRight, testCases)
+}
+
+func TestPad(t *testing.T) {
+	t.Parallel()
+
+	testCases := []utils.GenericTestCase[utils.ThreeArgumentTestCasesArgsType[string, int, string], string]{
+		{
+			Name: "Pad single character on both sides",
+			Args: utils.ThreeArgumentTestCasesArgsType[string, int, string]{A: "abc", B: 5, C: "0"},
+			Want: "0abc0",
+		},
+		{
+			Name: "Pad multiple characters on both sides",
+			Args: utils.ThreeArgumentTestCasesArgsType[string, int, string]{A: "abc", B: 6, C: "xy"},
+			Want: "xyabcx",
+		},
+		{
+			Name: "Original string longer than padding",
+			Args: utils.ThreeArgumentTestCasesArgsType[string, int, string]{A: "abc", B: 2, C: "0"},
+			Want: "abc",
+		},
+		{
+			Name: "Empty original string",
+			Args: utils.ThreeArgumentTestCasesArgsType[string, int, string]{A: "", B: 2, C: "0"},
+			Want: "00",
+		},
+		{
+			Name: "Empty padding string",
+			Args: utils.ThreeArgumentTestCasesArgsType[string, int, string]{A: "abc", B: 5, C: ""},
+			Want: "abc",
+		},
+		{
+			Name: "Empty input and padding",
+			Args: utils.ThreeArgumentTestCasesArgsType[string, int, string]{A: "", B: 0, C: ""},
+			Want: "",
+		},
+		{
+			Name: "Negative length",
+			Args: utils.ThreeArgumentTestCasesArgsType[string, int, string]{A: "abc", B: -1, C: "0"},
+			Want: "abc",
+		},
+	}
+
+	utils.RunThreeArgumentTestCases(t, "Pad", strutil.Pad, testCases)
+}
