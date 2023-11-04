2dfeece90904dc4e8cbf996c18819055001af0a8
GrewalAS <asgrewal@ualberta.ca>
2023-11-04 17:56:04 -0600

    Update throttle.go

--- functions/throttle.go
+++ functions/throttle.go
@@ -6,12 +6,12 @@ import "time"
 // The returned function will return the result of the first call until the duration has passed.
 // After the duration has passed, the function will be called again and the result will be returned.
 func Throttle[R any](f func(...any) R, d time.Duration) func(...any) R {
-	//var (
+	// var (
 	//	lastCalled time.Time
 	//	result     any
 	//)
 	return func(args ...any) R {
-		//if time.Since(lastCalled) > duration {
+		// if time.Since(lastCalled) > duration {
 		//	result = f(args)
 		//	lastCalled = time.Now()
 		//}
